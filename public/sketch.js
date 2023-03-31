e3a4471d8936bcecff5613bfb0ffedf6531b1257
Glitch (glitch-blank-node) <none>
2023-03-31 16:29:58 +0000

    🍏👐 Checkpoint ./public/sketch.js:55706575/2

--- public/sketch.js
+++ public/sketch.js
@@ -7,7 +7,7 @@ const COLORS = [
   '#e21400', '#91580f', '#f8a700', '#f78b00',
   '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
   '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
-];
+
 
 // Initialize variables
 
