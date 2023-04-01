6d547d76f694889e948c4c2516e08b6b6457d705
Glitch (glitch-blank-node) <none>
2023-04-01 02:05:05 +0000

    ⛷🛳 Checkpoint ./public/receiver.js:55706575/1796 ./public/receiver.html:55706575/1419 ./receiver.js:55706575/265

--- public/receiver.js
+++ public/receiver.js
@@ -1,20 +1,2 @@
 const socket = io();
-let input;
 
-function setup() {
-  createCanvas(100, 100);
-  background( 'pink' );
-  
-  socket.on("data", newMsg);
-
-  function newMsg(data) {
-    input = Int32Array(data);
-    print(Int32Array(data));
-  }
-}
-
-function draw() { 
-  print(input);
-
-
-}
